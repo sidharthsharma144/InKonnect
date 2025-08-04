50e3ebc65a6d4cfa124d72e13f7b24e7663e661e
sidharthsharma144 <sidharthsharma17240@gmail.com>
2025-08-04 22:26:41 +0530

    files setup done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className='bg-black text-amber-950'>hello</div>
+      <div className='bg-black text-amber-400'>hello</div>
     </>
   )
 }
-
-export default App
+export default App;
