1c14df47d17dc8ab022406bb4b5deb6bf587ea4d
sidharthsharma144 <sidharthsharma17240@gmail.com>
2025-08-06 23:03:03 +0530

    login form created

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,35 +1,61 @@
-import React from 'react'
-import './login.css'
-
+import React, { useState } from "react";
+import "./login.css";
 
 const Login = () => {
+  const [currState, setCurrState] = useState("Sign Up");
+
+  const toggleState = () => {
+    setCurrState(currState === "Sign Up" ? "Login" : "Sign Up");
+  };
+
   return (
-    <div className='login'>
-     <img src="" className="logo" alt="Logo" />
-     <form action="" className="login-form">
-      <h2>Sign Up</h2>
-      <input type="text" placeholder='Username' className='form-input' required />
-      <input type="email" placeholder='Email' className='form-input' required />
-      <input type="password" placeholder='Password' className='form-input' required />
-     
-     <button type='submit'>
-      Sign Up
-     </button>
-     <div className='login-term'>
-      <input type="checkbox" className="text" />
-      <p> Agree to the terms of use & privacy policy.
-      </p>
-     </div>
-     <div className="login-forgot">
-      <p className="login-toggle">
-        Already have an account <span>click here</span> 
-      </p>
-     </div>
+    <div className="login">
+      <img src="/logo.jpeg" className="logo" alt="Logo" />
+      <form className="login-form">
+        <h2>{currState}</h2>
 
-     </form>
+        {currState === "Sign Up" && (
+          <input
+            type="text"
+            placeholder="Username"
+            className="form-input"
+            required
+          />
+        )}
 
+        <input
+          type="email"
+          placeholder="Email"
+          className="form-input"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="form-input"
+          required
+        />
+
+        <button type="submit">{currState === "Sign up"?"Create account":"Login now"}</button>
+
+        {currState === "Sign Up" && (
+          <div className="login-term">
+            <input type="checkbox" className="text" required />
+            <p> Agree to the terms of use & privacy policy.</p>
+          </div>
+        )}
+
+        <div className="login-forgot">
+          <p className="login-toggle">
+            {currState === "Sign Up"
+              ? "Already have an account"
+              : "Don't have an account?"}{" "}
+            <span onClick={toggleState}>Click here</span>
+          </p>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
