8460aabb0e96b835a77675e4cad66fa9051c7ac4
sidharthsharma144 <sidharthsharma17240@gmail.com>
2025-08-06 22:36:46 +0530

    signup form ready

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,7 +5,28 @@ import './login.css'
 const Login = () => {
   return (
     <div className='login'>
-     <img src="/public/logo2.jpeg" alt="Logo" />
+     <img src="" className="logo" alt="Logo" />
+     <form action="" className="login-form">
+      <h2>Sign Up</h2>
+      <input type="text" placeholder='Username' className='form-input' required />
+      <input type="email" placeholder='Email' className='form-input' required />
+      <input type="password" placeholder='Password' className='form-input' required />
+     
+     <button type='submit'>
+      Sign Up
+     </button>
+     <div className='login-term'>
+      <input type="checkbox" className="text" />
+      <p> Agree to the terms of use & privacy policy.
+      </p>
+     </div>
+     <div className="login-forgot">
+      <p className="login-toggle">
+        Already have an account <span>click here</span> 
+      </p>
+     </div>
+
+     </form>
 
     </div>
   )
